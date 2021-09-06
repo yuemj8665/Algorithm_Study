d4376e7d17865724f5f39a9f3641515dc89a10f3
MJMJ92 <yuemjmahu@gmail.com>
2021-09-06 18:14:14 +0900

    2021-09-06 Prim_algorithm_class ...ing2

--- src/onlineClass/part_2/Prim_Algorithm_16.java
+++ src/onlineClass/part_2/Prim_Algorithm_16.java
@@ -26,7 +26,61 @@ public class Prim_Algorithm_16 {
         }
 
 
+
     }
+
+    public static class PrimPath {
+        public ArrayList<Edge> primFunc(String startNode, ArrayList<Edge> edges) {
+            Edge currentEdge, poppedEdge, adjacentEdgeNode;
+            ArrayList<Edge> currentEdgeList, candidateEdgeList, adjacentEdgeNodes;
+            PriorityQueue<Edge> priorityQueue;
+
+            ArrayList<String> connectedNodes = new ArrayList<String>();
+            ArrayList<Edge> mst = new ArrayList<Edge>();
+            HashMap<String, ArrayList<Edge>> adjacentEdges = new HashMap<>();
+            for (int index = 0; index < edges.size(); index++) {
+                currentEdge = edges.get(index);
+                if (!adjacentEdges.containsKey(currentEdge)) {
+                    adjacentEdges.put(currentEdge.node1, new ArrayList<Edge>());
+                }
+                if (!adjacentEdges.containsKey(currentEdge)) {
+                    adjacentEdges.put(currentEdge.node2, new ArrayList<Edge>());
+                }
+
+            }
+            for (int index = 0; index < edges.size(); index++) {
+                currentEdge = edges.get(index);
+                currentEdgeList = adjacentEdges.get(currentEdge.node1);
+                currentEdgeList.add(new Edge(currentEdge.weight, currentEdge.node1, currentEdge.node2));
+                currentEdgeList = adjacentEdges.get(currentEdge.node2);
+                currentEdgeList.add(new Edge(currentEdge.weight, currentEdge.node2, currentEdge.node1));
+            }
+            connectedNodes.add(startNode);
+            candidateEdgeList = adjacentEdges.getOrDefault(startNode, new ArrayList<Edge>());
+            priorityQueue = new PriorityQueue<Edge>();
+            for (int index = 0; index < candidateEdgeList.size(); index++) {
+                priorityQueue.add(candidateEdgeList.get(index));
+            }
+            while (priorityQueue.size() > 0) {
+                poppedEdge = priorityQueue.poll();
+                if (!connectedNodes.contains(poppedEdge.node2)) {
+                    // 해당 Edge를 mst에 추가한다
+                    connectedNodes.add(poppedEdge.node2);
+                    mst.add(new Edge(poppedEdge.weight, poppedEdge.node1, poppedEdge.node2));
+                    adjacentEdgeNodes = adjacentEdges.getOrDefault(poppedEdge.node2, new ArrayList<Edge>());
+                    for (int index = 0; index < adjacentEdgeNodes.size(); index++) {
+                        adjacentEdgeNode = adjacentEdgeNodes.get(index);
+                        if (!connectedNodes.contains(adjacentEdgeNode.node2)) {
+                            priorityQueue.add(adjacentEdgeNode);
+                        }
+                    }
+                }
+
+            }
+            return mst;
+        }
+    }
+
 
     public static void main(String[] args) {
         PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
@@ -46,6 +100,22 @@ public class Prim_Algorithm_16 {
         graph.containsKey("B");
 
         graph.getOrDefault("C", new ArrayList<Edge>());
+
+        ArrayList<Edge> myedges = new ArrayList<>();
+        myedges.add(new Edge(7, "A", "B"));
+        myedges.add(new Edge(5, "A", "D"));
+        myedges.add(new Edge(8, "B", "C"));
+        myedges.add(new Edge(9, "B", "D"));
+        myedges.add(new Edge(7, "D", "E"));
+        myedges.add(new Edge(5, "C", "E"));
+        myedges.add(new Edge(7, "B", "E"));
+        myedges.add(new Edge(6, "D", "F"));
+        myedges.add(new Edge(8, "E", "F"));
+        myedges.add(new Edge(9, "E", "G"));
+        myedges.add(new Edge(11, "F", "G"));
+
+        PrimPath primPath = new PrimPath();
+        System.out.println(primPath.primFunc("A", myedges));
 
     }
 }
